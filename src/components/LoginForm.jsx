4073700a9d05da05dd9f57633b899b547783d3a6
SGman98 <bastiangarnica@gmail.com>
2022-05-26 02:38:42 -0500

    Add basic LoginForm
    Co-authored-by: Kevin Gonzalez <kegonzalezs@unal.edu.co>

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,71 @@
+import "../assets/css/Forms.css";
+
+import { Form, Input, Button, Checkbox, Typography } from "antd";
+
+const { Item } = Form;
+const { Password } = Input;
+const { Title, Text } = Typography;
+
+const LoginForm = () => {
+	const handleSubmit = (values) => {
+		console.log(values);
+	};
+	return (
+		<>
+			<div className="containerPrincipal">
+				<div className="containerForm">
+					<Title level={4}>¡Bienvenido a SParking!</Title>
+					<Title level={5}>
+						Por favor llena todos los campos para ingresar:
+					</Title>
+					<Form
+						name="login"
+						labelCol={{ span: 8 }}
+						initialValues={{ remember: true }}
+						autoComplete="off"
+						onFinish={handleSubmit}
+					>
+						<Item
+							label="Email"
+							name="email"
+							rules={[
+								{ required: true, message: "Por favor ingresa tu usuario!" },
+								{
+									type: "email",
+									message: "Por favor ingresa un correo valido",
+								},
+							]}
+						>
+							<Input />
+						</Item>
+						<Item
+							label="Contraseña"
+							name="password"
+							rules={[
+								{ required: true, message: "Por favor ingresa tu contraseña!" },
+							]}
+						>
+							<Password />
+						</Item>
+						<Text>
+							<a href="/#">olvidaste tu contraseña?</a>
+						</Text>
+						<Item name="remember" valuePropName="checked">
+							<Checkbox>Recuerdame</Checkbox>
+						</Item>
+						<Item>
+							<Button type="primary" htmlType="submit">
+								Iniciar usuario
+							</Button>
+						</Item>
+					</Form>
+					<Text>
+						No tienes una cuenta? <a href="/users/register">Registrate!</a>
+					</Text>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default LoginForm;
