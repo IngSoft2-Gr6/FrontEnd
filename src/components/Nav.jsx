7954e2a25c1537a8d3f34928e9f19660662ea87e
SGman98 <bastiangarnica@gmail.com>
2022-05-26 16:57:32 -0500

    Improve `Nav` component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,25 @@
 import { Tabs, Tab } from "@mui/material";
 
 const Nav = () => {
+	const paths = [
+		{ label: "Home", href: "/home" },
+		{ label: "Login", href: "/users/login" },
+		{ label: "Signup", href: "/users/signup" },
+	];
+
+	const url = new URL(window.location.href);
+	const tab = paths.findIndex((path) => path.href === url.pathname);
+
 	return (
-		<Tabs>
-			<Tab label="Home" href="/home" />
-			<Tab label="Log In" href="/users/login" />
-			<Tab label="Sign Up" href="/users/signup" />
+		<Tabs value={tab}>
+			{paths.map((path, index) => (
+				<Tab
+					label={path.label}
+					href={path.href}
+					value={index}
+					key={path.label}
+				/>
+			))}
 		</Tabs>
 	);
 };
