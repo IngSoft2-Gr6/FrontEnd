e68c6afde03d356ab0189ee0e6b4cdefcc992c20
SGman98 <bastiangarnica@gmail.com>
2022-05-26 02:45:24 -0500

    Uncomment reportWebVitals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals";
 import LoginForm from "./components/LoginForm";
 
 import "antd/dist/antd.min.css";
@@ -10,10 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<LoginForm />
+		<SignupForm />
 	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
