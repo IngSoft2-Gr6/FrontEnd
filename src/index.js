11f435699bcae4c28b023dfa6fab9508214270f1
SGman98 <bastiangarnica@gmail.com>
2022-05-26 03:13:19 -0500

    Refactor import order

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import "./index.css";
+import "antd/dist/antd.min.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import LoginView from "./views/LoginView";
 import SignupView from "./views/SignupView";
@@ -11,8 +13,6 @@ import {
 	Routes,
 	Navigate,
 } from "react-router-dom";
-
-import "antd/dist/antd.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
