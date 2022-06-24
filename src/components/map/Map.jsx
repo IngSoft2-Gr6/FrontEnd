b08e170892449cdb0e69ad559ff24748bb776fef
kegonzalezs <kegonzalezs@unal.edu.co>
2022-06-24 15:48:46 -0500

    modifications in the design of the Popups

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -10,8 +10,6 @@ import {
 import API from "../../config/axios";
 import { Typography, Paper, Grid,Avatar } from "@mui/material";
 import { useEffect } from "react";
-import { gridSpacing } from './constant';
-import { LoginForm } from "../auth";
 
 
 
@@ -47,36 +45,36 @@ const Map = () =>{
 				<Marker position={{lat:parking.coords[0], lng:parking.coords[1]}}>
 					<Popup>
 						<Paper elevation={12} style={{ padding: "1rem", borderRadius: "1rem" }}>
-							<Grid container spacing={gridSpacing} width="100%">
+							<Grid container spacing="15" width="100%">
 								<Grid item xs={12}>
-									<Grid container spacing={gridSpacing}>
+									<Grid container spacing="50">
 										<Grid item lg={3} md={4} sm={4} xs={12}>
-											<Avatar>{parking.name.charAt(0)} </Avatar>
+											<Avatar style={{  top: "50%" ,left: "50%"}}>{parking.name.charAt(0)} </Avatar>
 										</Grid>
 										<Grid item lg={9} md={8} sm={8} xs={12}>
-											{parking.name}
-											<br/>
-											{parking.address}
+											<Typography>{parking.name}</Typography>
+											<Typography>{parking.address}</Typography>
 										</Grid>
 									</Grid>
 								</Grid>
 
 								<Grid item xs={12}>
-									<Grid container spacing={gridSpacing}>
+									<Grid container>
 										<Grid item lg={6} md={6} sm={6} xs={6}>
-											<Avatar><Typography>{parking.fee}</Typography></Avatar>
+											<Avatar><Typography >{parking.fee}</Typography></Avatar>
+											Tarifa
 										</Grid>
 										<Grid item lg={6} md={6} sm={6} xs={6}>
 											<Avatar><Typography>{parking.capacity}</Typography></Avatar>
-											
+											Capacidad
 										</Grid>
 									</Grid>
 								</Grid>
 
 								<Grid item xs={12}>
-									<Grid container spacing={gridSpacing}>
+									<Grid container spacing="3">
 										<Grid item lg={12} md={12} sm={12} xs={12}>
-											{parking.description}
+											<Typography>{parking.description}</Typography>
 										</Grid>
 									</Grid>
 								</Grid>
