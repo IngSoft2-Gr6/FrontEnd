1310021d98cac06b942324f9d61b6eb672884573
Nerón <54643791+Slopezpa@users.noreply.github.com>
2022-06-30 01:55:52 -0500

    Policies and social networks
    The social networks and policies were added.

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,12 @@
-import { Container, Toolbar, Typography } from "@mui/material";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Container, Link, Toolbar, Typography } from "@mui/material";
 import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
 import { styled } from "@mui/material/styles";
+import InstagramIcon from '@mui/icons-material/Instagram';	
+import TwitterIcon from '@mui/icons-material/Twitter';
+import IconButton from '@mui/material/IconButton';
+import Fingerprint from '@mui/icons-material/Fingerprint';
 
 const Footers = styled("footer")(({ theme }) => ({
 	position: "fixed",
@@ -12,11 +18,13 @@ const Footers = styled("footer")(({ theme }) => ({
 	color: theme.palette.text.primary,
 }));
 
+
 const Footer = () => {
 	return (
 		<Footers>
 			<Container maxWidth="xl">
-				<Toolbar disableGutters>
+				<Toolbar>
+
 					<DirectionsCarIcon sx={{ mr: 1 }} />
 					<Typography
 						variant="h5"
@@ -24,8 +32,7 @@ const Footer = () => {
 						component="a"
 						href="/home"
 						sx={{
-							mr: 2,
-							flexGrow: 1,
+							mr:2,
 							fontFamily: "monospace",
 							fontWeight: 700,
 							letterSpacing: ".3rem",
@@ -35,10 +42,28 @@ const Footer = () => {
 					>
 						SPARKING
 					</Typography>
+					<Stack sx={{display:{md : "flex"}}} spacing={4} direction="row">
+				<Link href="https://www.instagram.com/sparking.oficial/" zIndex={1} left="67%" top="20%" position="absolute">
+					<IconButton aria-label="Instagram" >
+        		<InstagramIcon />
+      		</IconButton>
+				</Link>
+				<Link href="https://twitter.com/sparking_694" zIndex={1}  left="55%"  top="20%" position="absolute">
+      		<IconButton aria-label="Twitter"  >
+        		<TwitterIcon />
+      		</IconButton>
+					</Link>
+					<Link  href="https://drive.google.com/file/d/12Psmi4btB9-VU5dMl5Rm0zBDXneqZ5k0/view" zIndex={1}  left="45%"  top="20%" position="absolute">
+      		<IconButton aria-label="fingerprint"  >
+        		<Fingerprint />
+      		</IconButton>
+					</Link>
+    		</Stack>
 				</Toolbar>
 			</Container>
 		</Footers>
 	);
 };
+
 
 export default Footer;
